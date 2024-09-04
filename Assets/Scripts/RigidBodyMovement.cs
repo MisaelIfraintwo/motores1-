a01d07ba2d1928992683b0f5f0c9267112a452cf
MisaelIfrain <misael.gman@gmail.com>
2024-09-04 00:56:30 -0300

    small fixes

--- Assets/Scripts/RigidBodyMovement.cs
+++ Assets/Scripts/RigidBodyMovement.cs
@@ -2,33 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveObject : MonoBehaviour
+public class RigidBodyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public Vector3 newPosition;
-
     public float speed;
 
     public Vector2 inputVector;
+    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Game start");
-
         
     }
 
     // Update is called once per frame
     void Update()
     {
-
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
 
-        transform.Translate(inputVector.x * speed, 0f, inputVector.y * speed);
-       
-
-        Debug.Log("GAme running");
-        if (Input.GetKeyDown(KeyCode.P)) ;
-      
     }
 }
