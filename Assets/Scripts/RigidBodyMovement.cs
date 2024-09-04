97c918279b8edcc6d4244e5afdb3ba5cfa35ffb4
MisaelIfrain <misael.gman@gmail.com>
2024-09-04 01:07:55 -0300

    Update RigidBodyMovement.cs

--- Assets/Scripts/RigidBodyMovement.cs
+++ Assets/Scripts/RigidBodyMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class RigidBodyMovement : MonoBehaviour
 {
     public float speed;
+    public Rigidbody rigidBody;
 
     public Vector2 inputVector;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        rigidBody = GetComponent<Rigidbody>();  
     }
 
     // Update is called once per frame
@@ -18,6 +19,6 @@ public class RigidBodyMovement : MonoBehaviour
     {
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
-
+        rigidBody.AddForce(inputVector.x * speed, 0f, inputVector.y * speed, ForceMode.Impulse);
     }
 }
